6a9c3f3bb367e3eb273e92ea2f4b947f9ec963cd
rqs <qhapaq49@yahoo.co.jp>
2020-06-27 13:06:05 +0900

    add convert_bin and option for draw positions

--- src/misc.h
+++ src/misc.h
@@ -29,6 +29,9 @@
 #include <ostream>
 #include <string>
 #include <vector>
+#ifndef _MSC_VER
+#include <mm_malloc.h>
+#endif
 
 #include "types.h"
 #include "thread_win32_osx.h"
@@ -125,6 +128,20 @@ inline std::ostream& operator<<(std::ostream& os, PRNG& prng)
 {
   os << "PRNG::seed = " << std::hex << prng.get_seed() << std::dec;
   return os;
+}
+
+inline uint64_t mul_hi64(uint64_t a, uint64_t b) {
+#if defined(__GNUC__) && defined(IS_64BIT)
+    __extension__ typedef unsigned __int128 uint128;
+    return ((uint128)a * (uint128)b) >> 64;
+#else
+    uint64_t aL = (uint32_t)a, aH = a >> 32;
+    uint64_t bL = (uint32_t)b, bH = b >> 32;
+    uint64_t c1 = (aL * bL) >> 32;
+    uint64_t c2 = aH * bL + c1;
+    uint64_t c3 = aL * bH + (uint32_t)c2;
+    return aH * bH + (c2 >> 32) + (c3 >> 32);
+#endif
 }
 
 /// Under Windows it is not possible for a process to run on more than one
